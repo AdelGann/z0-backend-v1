d32f738789906207c247037c1b58ccd41ffd8a7f
Adel Gannem <imadelg14@gmail.com>
2025-05-18 16:42:34 -0400

    feat: add middleware at main app to prevent panic errors

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/AdelGann/z0-backend-v1/internal/routes"
 	"github.com/AdelGann/z0-backend-v1/config"
+	"github.com/AdelGann/z0-backend-v1/internal/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/log"
+	fiberlog "github.com/gofiber/fiber/v2/log"
+	stdlog "log"
 )
 
 func main() {
@@ -14,8 +15,21 @@ func main() {
 	// initialize app
 	app := fiber.New()
 
+	// middleware to prevent panic errors
+	app.Use(func(c *fiber.Ctx) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				stdlog.Printf("Recovered from panic: %v", r)
+				err = c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+					"error": r,
+				})
+			}
+		}()
+		return c.Next()
+	})
+
 	// user routes
 	routes.MainRoutes(app)
 
-	log.Fatal(app.Listen(":3000"))
+	fiberlog.Fatal(app.Listen(":3000"))
 }
